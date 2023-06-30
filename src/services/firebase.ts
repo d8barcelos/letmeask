5c5478aa36c79af059bca31461d3051a6e748cdb
Diogo <d8barcelos@gmail.com>
2023-06-30 15:25:01 -0300

    commit

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 
 import 'firebase/auth';
 import 'firebase/database';
@@ -15,5 +15,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth;
+export const auth = firebase.auth();
 export const database = firebase.database();
